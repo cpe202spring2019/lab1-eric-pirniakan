7c039858429a7bae9a8922881e6cf2349f8c6f83
Eric Pirniakan <ericpirniakan@Erics-MacBook-Pro-2.local>
2019-04-10 07:31:55 -0700

    finished lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,5 @@
+#Name: Eric Pirniakan
+
 def max_list_iter(int_list):  # must use iteration not recursion
     if int_list == []:
         return None
@@ -19,9 +21,21 @@ def reverse_rec(int_list):   # must use recursion
     else:
         return [int_list[len(int_list)-1]] + reverse_rec(int_list[:len(int_list)-1])
 
-'''def bin_search(target, low, high, int_list):  # must use recursion
+def bin_search(target, low, high, int_list):  # must use recursion
     if int_list is None:
         raise ValueError
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-''' 
+    if int_list == []:
+        return None
+    if low > high:
+        return None
+    elif low <= high:
+        middle = (low + high) // 2
+        if int_list[middle] == target:
+            return middle
+        elif int_list[middle] > target:
+            high = middle - 1
+            return bin_search(target, low, high, int_list)
+        elif int_list[middle] < target:
+            low = middle + 1
+            return bin_search(target, low, high, int_list)
+
