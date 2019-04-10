86d5ce44c2497c036204ad05a3ecd853284dcfd1
Eric Pirniakan <ericpirniakan@Erics-MacBook-Pro-2.local>
2019-04-10 07:32:21 -0700

    finished lab1_test_cases.py

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -50,13 +50,53 @@ class TestLab1(unittest.TestCase):
     def test_reverse_rec_all_same(self):
         self.assertEqual(reverse_rec([9, 9, 9, 9, 9, 9, 9, 9]), [9, 9, 9, 9, 9, 9, 9, 9])
 
-
-    '''def test_bin_search(self):
+    def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4)
-'''
+        
+    def test_bin_search_empty(self):
+        list_val = []
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(5, 0, len(list_val)-1, list_val), None)
+        
+    def test_bin_search_none(self):
+        tlist = None
+        with self.assertRaises(ValueError):  #used to check for exception
+            bin_search(1, 0, 5, tlist)
+            
+    def test_bin_search_even(self):
+        list_val2 = [1, 2, 8, 15, 25, 59]
+        low = 0
+        high = len(list_val2)-1
+        self.assertEqual(bin_search(2, 0, len(list_val2)-1, list_val2), 1)
+     
+    def test_bin_search_repeated_numbers(self): #returns the first occurrence of the repeated number
+        list_val = [2, 5, 5, 5, 5, 7, 10]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(5, 0, len(list_val)-1, list_val), 3)
+        
+    def test_bin_search_not_in_list(self):
+        list_val = [12, 32, 52, 82, 102]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(5, 0, len(list_val)-1, list_val), None)
+        
+    def test_bin_search_lower_half(self):
+        list_val = [10, 20, 30, 40, 50, 60, 70]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(20, 0, len(list_val)-1, list_val), 1)
+        
+    def test_bin_search_upper_half(self):
+        list_val = [10, 20, 30, 40, 50, 60, 70]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(60, 0, len(list_val)-1, list_val), 5)
+        
 if __name__ == "__main__":
         unittest.main()
 
